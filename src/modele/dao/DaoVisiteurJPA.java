cd6f1b4bc1c813f7bbbc31e73165defcb5ff73c6
btssio <btssio@btssio-VirtualBox>
2014-12-11 17:19:53 +0100

    Ajout de la fonction vérifierlogin pour se connecter

--- src/modele/dao/DaoVisiteurJPA.java
+++ src/modele/dao/DaoVisiteurJPA.java
@@ -3,10 +3,9 @@
  */
 package modele.dao;
 
+import java.text.*;
 import java.util.*;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceException;
-import javax.persistence.Query;
+import javax.persistence.*;
 import modele.metier.Visiteur;
 
 
@@ -19,12 +18,27 @@ public class DaoVisiteurJPA {
      * @param matricule : identifiant technique
      * @return une instance de la classe Visiteur
      */
-    public static Visiteur selectOne(EntityManager em, Long matricule) throws PersistenceException {
+    public static Visiteur selectOne(EntityManager em, String matricule) throws PersistenceException {
         Visiteur unVisiteur = null;
         unVisiteur = em.find(Visiteur.class, matricule);
         return unVisiteur;
     }
     
+    
+    /**
+     * selectOneL : lire un enregistrement dans la table Visiteur
+     *
+     * @param em : contexte de persistance
+     * @param login : nom du visiteur
+     * @return une instance de la classe Visiteur
+     */
+    public static Visiteur selectOneByLogin(EntityManager em, String login) throws PersistenceException {
+        Visiteur visiteur = null;
+        Query query = em.createQuery("select v from Visiteur v where v.vis_nom = :login");
+        query.setParameter("login", login);
+        visiteur = (Visiteur) query.getSingleResult();
+        return visiteur;
+    }
 
     /**
      * lire tous les enregistrements de la table Visiteur
@@ -37,6 +51,27 @@ public class DaoVisiteurJPA {
         Query query= em.createQuery("select v from Visiteur v");
         lesVisiteurs = query.getResultList();
         return lesVisiteurs;
+    }
+    
+    /**
+     * Vérifier le login d'un Visiteur
+     *
+     * @param em : contexte de persistance
+     * @param login: nom du visiteur
+     * @param mdp: date d'embauche avec format JJ-MMM-AA
+     * @return une valeur boolean
+     */
+    public static boolean verifierLoginMdp(EntityManager em,  String login, String mdp) throws PersistenceException {
+        boolean ok=false;
+        Visiteur unVisiteur = selectOneByLogin(em, login);
+        Format formatter = new SimpleDateFormat("dd-MMM-yyyy");
+        String dateEmbauche = formatter.format(unVisiteur.getDateEmbauche());
+        
+        if(dateEmbauche==mdp){
+            ok=true;
+        }
+        
+        return ok;
     }
 
 
